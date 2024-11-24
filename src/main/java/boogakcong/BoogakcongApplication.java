2b7a71f2f438912b6536b38fe725b8c2d67a056e
stopmin <geemin0@naver.com>
2024-11-24 12:37:28 +0900

    Project Initialization author stopmin <geemin0@naver.com> 1732419448 +0900 committer stopmin <geemin0@naver.com> 1732419448 +0900
    Project Initialization
    
    Project Initialization
    
    Project Initialization
    
    Project Initialization
    
    Project Initialization
    
    Project Initialization
    
    Project Initialization
    
    Project Initialization

---
+++ src/main/java/boogakcong/BoogakcongApplication.java
@@ -0,0 +1,13 @@
+package boogakcong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoogakcongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoogakcongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boogakcong/BoogakcongApplicationTests.java
@@ -0,0 +1,13 @@
+package boogakcong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoogakcongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
